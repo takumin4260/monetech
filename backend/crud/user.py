ccdb0ddd04200f519e816c2ad022a097c311794b
Pictogram23 <shanchengyinger@gmail.com>
2025-09-02 11:31:52 +0900

    usersテーブルのCRを実装

--- backend/crud/user.py
+++ backend/crud/user.py
@@ -3,12 +3,14 @@ from models.user import User
 from schemas.user import UserCreate
 
 
-def get_user(db: Session, user_id: int):
+def get_user_by_id(db: Session, user_id: int):
     return db.query(User).filter(User.id == user_id).first()
 
 
-def create_user(db: Session, user: UserCreate):
-    db_user = User(name=user.name)
+def create_user(db: Session, user: UserCreate) -> User:
+    db_user = User(
+        name=user.name, icon=user.icon, email=user.email, password=user.password
+    )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
