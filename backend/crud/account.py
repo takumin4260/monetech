cc5423547a640648c43273bbfed7446b1045560e
den3asphalt <86176261+den3asphalt@users.noreply.github.com>
2025-09-02 12:24:22 +0900

    depositが0になる問題を修正

--- backend/crud/account.py
+++ backend/crud/account.py
@@ -10,8 +10,8 @@ def get_account_by_user_id(db: Session, user_id: int) -> Account | None:
 def get_account_by_account_number(db: Session, account_number: int) -> Account | None:
     return db.query(Account).filter(Account.account_number == account_number).first()
 
-def create_account(db: Session, account: AccountCreate, deposit: int=0) -> Account:
-    db_account = Account(user_id=account.user_id, deposit=deposit)
+def create_account(db: Session, account: AccountCreate) -> Account:
+    db_account = Account(user_id=account.user_id, deposit=account.deposit)
     db.add(db_account)
     db.commit()
     db.refresh(db_account)
