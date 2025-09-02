187a5cfdc23b36cd623e77d59b0b1cc9e03fe9ae
Pictogram23 <shanchengyinger@gmail.com>
2025-09-02 16:48:46 +0900

    ログイン、ログアウトを実装

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI
 from database import engine, Base
-from routers import user, account, request, send, api
+from routers import user, account, request, send, api, auth
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(SessionMiddleware, secret_key="your_secret_key")
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router)
 app.include_router(send.router)
 app.include_router(account.router)
 app.include_router(api.router)
 app.include_router(request.router)
+app.include_router(auth.router)
