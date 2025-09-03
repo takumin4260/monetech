1bde5e0ca4dccf927d9156b2ff32f46ecb369b91
den3asphalt <86176261+den3asphalt@users.noreply.github.com>
2025-09-03 15:12:51 +0900

    users/<user-id>を取得できるように

--- frontend/src/app/api/getUser/[id]/route.ts
+++ frontend/src/app/api/getUser/[id]/route.ts
@@ -4,20 +4,18 @@ import  {NextResponse} from "next/server"
 // contextを引数として受け取る
 export async function GET(request: Request, context: { params: { id: string } }) {
   console.log("Fetching user with ID:", context.params);
-  // paramsオブジ ェクトからidを取得
-  const { id } = await context.params;
-
-  console.log("Fetching user with ID:", id);
+  // paramsオブジェクトからidを取得
+  const {id} = context.params;
 
   const upstream = await fetch(
     // 取得したidをURLに埋め込む
     `http://localhost:8000/users/${id}`
   );
 
-  console.log("Upstream response status:", upstream);
-
   // .json()はPromiseを返すのでawaitが必要
   const data = await upstream.json();
+
+  console.log("Fetched user data:", data);
 
   return NextResponse.json(data);
 }
