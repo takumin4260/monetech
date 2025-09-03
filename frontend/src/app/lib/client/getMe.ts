3e72b174bd659d8b4706c266b084feff96f19d90
木下匠 <takumin4260@gmail.com>
2025-09-03 16:34:28 +0900

    home rollback

--- frontend/src/app/lib/client/getMe.ts
+++ frontend/src/app/lib/client/getMe.ts
@@ -3,7 +3,7 @@ import { components } from "@/app/gen/schema";
 type MeResponse = components["schemas"]["MeResponse"];
 
 export async function getMe(): Promise<MeResponse> {
-  const response = await fetch("http://localhost:3000/api/getMe");
+  const response = await fetch("http://localhost:8000/me");
   const data = await response.json();
   console.log("[debug] response", data);
   return data;
