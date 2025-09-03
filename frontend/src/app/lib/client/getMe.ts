57157ac33ccd581584e2ba9934912956d2dfd3cc
Pictogram23 <shanchengyinger@gmail.com>
2025-09-03 16:56:28 +0900

    ユーザデータの表示完了

--- frontend/src/app/lib/client/getMe.ts
+++ frontend/src/app/lib/client/getMe.ts
@@ -3,7 +3,10 @@ import { components } from "@/app/gen/schema";
 type MeResponse = components["schemas"]["MeResponse"];
 
 export async function getMe(): Promise<MeResponse> {
-  const response = await fetch("http://localhost:8000/me");
+  const response = await fetch("http://localhost:8000/me", {
+    credentials: "include",
+  });
+  console.log(response);
   const data = await response.json();
   console.log("[debug] response", data);
   return data;
