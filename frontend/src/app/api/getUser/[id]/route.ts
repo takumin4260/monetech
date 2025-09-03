65501ec937f8ddc9b93b649552b622d7986dd6a1
den3asphalt <86176261+den3asphalt@users.noreply.github.com>
2025-09-03 14:55:16 +0900

    userAPI通った

--- frontend/src/app/api/getUser/[id]/route.ts
+++ frontend/src/app/api/getUser/[id]/route.ts
@@ -2,16 +2,19 @@
 import  {NextResponse} from "next/server"
 
 // contextを引数として受け取る
-export async function GET(
-    {params}: { params: { id: string } }
-) {
-  // paramsオブジェクトからidを取得
-  const { id } = params;
+export async function GET(request: Request, context: { params: { id: string } }) {
+  console.log("Fetching user with ID:", context.params);
+  // paramsオブジ ェクトからidを取得
+  const { id } = await context.params;
+
+  console.log("Fetching user with ID:", id);
 
   const upstream = await fetch(
     // 取得したidをURLに埋め込む
     `http://localhost:8000/users/${id}`
   );
+
+  console.log("Upstream response status:", upstream);
 
   // .json()はPromiseを返すのでawaitが必要
   const data = await upstream.json();
