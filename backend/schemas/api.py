60cc8171ff503624e4c770dd6aa62a113097b67e
Pictogram23 <shanchengyinger@gmail.com>
2025-09-03 11:18:20 +0900

    transfersエンドポイントのスキーマを実装

--- backend/schemas/api.py
+++ backend/schemas/api.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, NonNegativeInt
 from schemas.user import UserPublic
 from schemas.account import AccountPublic
+from datetime import datetime
 
 
 class MeResponse(BaseModel):
@@ -10,3 +11,19 @@ class MeResponse(BaseModel):
 
 class UserResponse(BaseModel):
     user: UserPublic
+
+
+class TransfersRequest(BaseModel):
+    to_user_id: int
+    money: NonNegativeInt
+    message: str | None
+
+
+class TransfersResponse(BaseModel):
+    id: int
+    from_user_id: int
+    to_user_id: int
+    money: NonNegativeInt
+    message: str | None
+    date: datetime
+    completed: bool
