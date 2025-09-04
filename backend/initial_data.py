c7d96245d261c06b8065a81c896cb55349c6d2f3
Pictogram23 <shanchengyinger@gmail.com>
2025-09-04 11:30:21 +0900

    ダミーデータのセット

--- backend/initial_data.py
+++ backend/initial_data.py
@@ -2,6 +2,9 @@ from sqlalchemy.orm import Session
 from crud.user import create_user
 from schemas.user import UserCreate
 from models.user import User
+from crud.account import create_account
+from schemas.account import AccountCreate
+from models.account import Account
 
 
 def create_initial_data(db: Session):
@@ -9,6 +12,28 @@ def create_initial_data(db: Session):
 
     if existing_user is None:
         user1 = UserCreate(
-            name="hoge", icon="hoge", email="hoge@example.com", password="hoge"
+            name="太郎", icon="taro.png", email="taro@example.com", password="taro"
         )
         create_user(db, user1)
+        
+        user2 = UserCreate(
+            name="花子", icon="hanako.png", email="hanako@example.com", password="hanako"
+        )
+        create_user(db, user2)
+        
+        user3 = UserCreate(
+            name="次郎", icon="jiro.png", email="jiro@example.com", password="jiro"
+        )
+        create_user(db, user3)
+
+    existing_account = db.query(Account).first()
+    
+    if existing_account is None:
+        account1 = AccountCreate(user_id=1, deposit=10000)
+        create_account(db, account1)
+        
+        account2 = AccountCreate(user_id=2, deposit=15000)
+        create_account(db, account2)
+        
+        account3 = AccountCreate(user_id=3, deposit=20000)
+        create_account(db, account3)
