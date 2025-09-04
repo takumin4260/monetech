014ba5015ee0423dca9e83f7ecf52b919a78422b
Pictogram23 <shanchengyinger@gmail.com>
2025-09-04 11:39:02 +0900

    コード整形を実行

--- backend/routers/api.py
+++ backend/routers/api.py
@@ -116,4 +116,6 @@ def billing(
         ),
     )
 
-    return BillingResponse(url=f"http://localhost:3000/send/users/{current_user.id}?money={db_request.money}&mode=billing")
+    return BillingResponse(
+        url=f"http://localhost:3000/send/users/{current_user.id}?money={db_request.money}&mode=billing"
+    )
