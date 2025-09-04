caf54ce7b99255494b6317659909b5fb4fb52fa7
takumin4260 <157480636+takumin4260@users.noreply.github.com>
2025-09-04 10:17:18 +0900

    connect GET user/{id} (#32)
    * connect user/{id}
    
    * change margin

--- frontend/src/app/lib/client/getUser.ts
+++ frontend/src/app/lib/client/getUser.ts
@@ -1,6 +1,4 @@
-import { components } from "@/app/gen/schema";
-
 export async function getUser(id: string){
-  const response = await fetch(`http://localhost:3000/api/getUser/${id}`);
+  const response = await fetch(`http://localhost:8000/users/${id}`);
   return await response.json();
 }
