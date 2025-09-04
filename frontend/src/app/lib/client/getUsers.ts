bdb3ee24debda867d6bcb538f32f6b9de2eea221
takumin4260 <157480636+takumin4260@users.noreply.github.com>
2025-09-04 11:36:21 +0900

    users 解消 (#39)

--- frontend/src/app/lib/client/getUsers.ts
+++ frontend/src/app/lib/client/getUsers.ts
@@ -1,8 +1,8 @@
-import { components } from "@/app/gen/schema";
+// import { components } from "@/app/gen/schema";
 
-type User = components["schemas"]["User"];
+// type User = components["schemas"]["User"];
 
-export async function getUsers(): Promise<User[]> {
-  const response = await fetch("http://localhost:3000/api/getUsers");
+export async function getUsers() {
+  const response = await fetch(`http://localhost:8000/users/`);
   return await response.json();
 }
